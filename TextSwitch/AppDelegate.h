c1067c81f24f232a01399a7aade87d5f3fc3f111
Vasily Popov <vasily.popov.it@gmail.com>
2017-08-10 21:53:21 +0300

    Initial Commit

---
+++ TextSwitch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TextSwitch
+//
+//  Created by Vasily Popov on 8/10/17.
+//  Copyright © 2017 Vasily Popov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TextSwitch/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TextSwitch
+//
+//  Created by Vasily Popov on 8/10/17.
+//  Copyright © 2017 Vasily Popov. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
